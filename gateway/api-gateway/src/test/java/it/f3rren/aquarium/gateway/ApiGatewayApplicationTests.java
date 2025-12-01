fc1cd94143ed785bffa343b878358069794cb84c
Samuele Alessandro Di Silvestri <samueledisilvestri@gmail.com>
2025-12-01 16:53:30 +0100

    feat: implement API Gateway with routing to microservices and CORS configuration

---
+++ gateway/api-gateway/src/test/java/it/f3rren/aquarium/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package it.f3rren.aquarium.gateway;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
