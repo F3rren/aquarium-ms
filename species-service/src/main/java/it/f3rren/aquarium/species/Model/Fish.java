45c88e36f6022d8534ceea8193ecd1f2e3ed90a3
Samuele Alessandro Di Silvestri <samueledisilvestri@gmail.com>
2025-12-01 09:33:39 +0100

    Add aquariums-service module with initial setup, including Maven configuration, application properties, and basic Spring Boot application structure; implement Coral and Fish models with Lombok annotations for boilerplate reduction.

--- species-service/src/main/java/it/f3rren/aquarium/species/Model/Fish.java
+++ species-service/src/main/java/it/f3rren/aquarium/species/Model/Fish.java
@@ -3,9 +3,11 @@ package it.f3rren.aquarium.species.Model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
