d1babf4ff2a475abf12d1b01881050324dd39695
Samuele Alessandro Di Silvestri <samueledisilvestri@gmail.com>
2025-12-01 14:23:21 +0100

    Implement water parameters service with initial setup, including Dockerfile, application properties, model, and main application class; add Maven wrapper and configuration files.

---
+++ water_parameters-service/src/main/java/it/f3rren/aquarium/water_parameters_service/Model/Parameter.java
@@ -0,0 +1,41 @@
+package it.f3rren.aquarium.water_parameters_service.Model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "water_parameters")
+public class Parameter {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    
+    @Column(name = "aquarium_id", nullable = false)
+    private Long aquariumId;
+    
+    @Column(name = "temperature", nullable = false)
+    private Double temperature;
+    
+    @Column(name = "ph", nullable = false)
+    private Double ph;
+    
+    @Column(name = "salinity", nullable = false)
+    private Integer salinity;
+    
+    @Column(name = "orp", nullable = false)
+    private Integer orp;
+    
+    @CreationTimestamp
+    @Column(name = "measured_at", nullable = false)
+    private LocalDateTime measuredAt;
+}
---
+++ water_parameters-service/src/main/java/it/f3rren/aquarium/water_parameters_service/WaterParametersServiceApplication.java
@@ -0,0 +1,13 @@
+package it.f3rren.aquarium.water_parameters_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterParametersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterParametersServiceApplication.class, args);
+	}
+
+}
