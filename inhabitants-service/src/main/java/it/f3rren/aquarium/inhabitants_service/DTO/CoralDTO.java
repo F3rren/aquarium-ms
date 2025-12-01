4d52ae404640df6edc8b08c158943c373004f40f
Samuele Alessandro Di Silvestri <samueledisilvestri@gmail.com>
2025-12-01 11:10:32 +0100

    Update application properties and DTOs for consistency; change server ports and update ID types in CoralDTO and FishDTO.

--- inhabitants-service/src/main/java/it/f3rren/aquarium/inhabitants_service/DTO/CoralDTO.java
+++ inhabitants-service/src/main/java/it/f3rren/aquarium/inhabitants_service/DTO/CoralDTO.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class CoralDTO {
-    private int id;
+    private Long id;
     private String commonName;
     private String scientificName;
     private String type;
