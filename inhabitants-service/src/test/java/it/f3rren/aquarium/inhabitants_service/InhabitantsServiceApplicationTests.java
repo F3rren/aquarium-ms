79cbccd9872635842d2098b0ee5d90ae96e83b87
Samuele Alessandro Di Silvestri <samueledisilvestri@gmail.com>
2025-12-01 10:25:18 +0100

    Add inhabitants-service module with initial setup, including Dockerfile, Maven configuration, application properties, and basic Spring Boot application structure; implement InhabitantsServiceApplication and its tests.

---
+++ inhabitants-service/src/test/java/it/f3rren/aquarium/inhabitants_service/InhabitantsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.f3rren.aquarium.inhabitants_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InhabitantsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
