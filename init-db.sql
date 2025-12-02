16e08983ed39f84602b008df4e9d4faf6daaa688
F3rren <93771919+F3rren@users.noreply.github.com>
2025-12-02 10:25:12 +0100

    Update init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -1,9 +1,9 @@
--- Creazione degli schemi per i microservizi
+-- Creation of schemas for the microservices
 CREATE SCHEMA IF NOT EXISTS core;
 CREATE SCHEMA IF NOT EXISTS inhabitants;
 CREATE SCHEMA IF NOT EXISTS parameters;
 
--- Grant dei permessi
+-- Granting permissions
 GRANT ALL PRIVILEGES ON SCHEMA core TO postgres;
 GRANT ALL PRIVILEGES ON SCHEMA inhabitants TO postgres;
 GRANT ALL PRIVILEGES ON SCHEMA parameters TO postgres;
