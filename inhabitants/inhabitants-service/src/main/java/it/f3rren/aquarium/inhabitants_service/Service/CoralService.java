0c59c5f298ba679a139c51636d12058445d577c6
Samuele Alessandro Di Silvestri <samueledisilvestri@gmail.com>
2025-12-02 17:25:28 +0100

    refactor: update species service URL and improve endpoint paths in inhabitants service

--- inhabitants/inhabitants-service/src/main/java/it/f3rren/aquarium/inhabitants_service/Service/CoralService.java
+++ inhabitants/inhabitants-service/src/main/java/it/f3rren/aquarium/inhabitants_service/Service/CoralService.java
@@ -1,5 +1,7 @@
 package it.f3rren.aquarium.inhabitants_service.Service;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -13,7 +15,7 @@ public class CoralService {
     
     private final RestTemplate restTemplate;
     
-    @Value("${species.service.url}")
+    @Value("${species.service.url:http://species-service:8083/species}")
     private String speciesServiceUrl;
     
     public CoralService(RestTemplate restTemplate) {
@@ -22,8 +24,29 @@ public class CoralService {
     
     public CoralDTO getCoralById(Long id) {
         try {
-            String url = speciesServiceUrl + "/api/species/corals/" + id;
-            return restTemplate.getForObject(url, CoralDTO.class);
+            String url = speciesServiceUrl + "/corals/" + id;
+            @SuppressWarnings("unchecked")
+            Map<String, Object> response = restTemplate.getForObject(url, Map.class);
+            if (response != null && response.containsKey("data")) {
+                @SuppressWarnings("unchecked")
+                Map<String, Object> data = (Map<String, Object>) response.get("data");
+                CoralDTO coral = new CoralDTO();
+                coral.setId(((Number) data.get("id")).longValue());
+                coral.setCommonName((String) data.get("commonName"));
+                coral.setScientificName((String) data.get("scientificName"));
+                coral.setType((String) data.get("type"));
+                coral.setMinTankSize(data.get("minTankSize") != null ? ((Number) data.get("minTankSize")).intValue() : 0);
+                coral.setMaxSize(data.get("maxSize") != null ? ((Number) data.get("maxSize")).intValue() : 0);
+                coral.setDifficulty((String) data.get("difficulty"));
+                coral.setLightRequirement((String) data.get("lightRequirement"));
+                coral.setFlowRequirement((String) data.get("flowRequirement"));
+                coral.setPlacement((String) data.get("placement"));
+                coral.setAggressive(data.get("aggressive") != null ? (Boolean) data.get("aggressive") : false);
+                coral.setFeeding((String) data.get("feeding"));
+                coral.setDescription((String) data.get("description"));
+                return coral;
+            }
+            throw new ResourceNotFoundException("Coral not found with id: " + id);
         } catch (HttpClientErrorException.NotFound e) {
             throw new ResourceNotFoundException("Coral not found with id: " + id);
         }
--- inhabitants/inhabitants-service/src/main/java/it/f3rren/aquarium/inhabitants_service/Service/FishService.java
+++ inhabitants/inhabitants-service/src/main/java/it/f3rren/aquarium/inhabitants_service/Service/FishService.java
@@ -1,5 +1,7 @@
 package it.f3rren.aquarium.inhabitants_service.Service;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -13,7 +15,7 @@ public class FishService {
     
     private final RestTemplate restTemplate;
     
-    @Value("${species.service.url}")
+    @Value("${species.service.url:http://species-service:8083/species}")
     private String speciesServiceUrl;
     
     public FishService(RestTemplate restTemplate) {
@@ -22,8 +24,32 @@ public class FishService {
     
     public FishDTO getFishById(Long id) {
         try {
-            String url = speciesServiceUrl + "/api/species/fishs/" + id;
-            return restTemplate.getForObject(url, FishDTO.class);
+            String url = speciesServiceUrl + "/fish/" + id;
+            
+            @SuppressWarnings("unchecked")
+            Map<String, Object> response = restTemplate.getForObject(url, Map.class);
+            
+            if (response != null && response.containsKey("data")) {
+                @SuppressWarnings("unchecked")
+                Map<String, Object> data = (Map<String, Object>) response.get("data");
+                
+                FishDTO fish = new FishDTO();
+                fish.setId(((Number) data.get("id")).longValue());
+                fish.setCommonName((String) data.get("commonName"));
+                fish.setScientificName((String) data.get("scientificName"));
+                fish.setFamily((String) data.get("family"));
+                fish.setMinTankSize(data.get("minTankSize") != null ? ((Number) data.get("minTankSize")).intValue() : 0);
+                fish.setMaxSize(data.get("maxSize") != null ? ((Number) data.get("maxSize")).intValue() : 0);
+                fish.setDifficulty((String) data.get("difficulty"));
+                fish.setReefSafe(data.get("reefSafe") != null ? (Boolean) data.get("reefSafe") : false);
+                fish.setTemperament((String) data.get("temperament"));
+                fish.setDiet((String) data.get("diet"));
+                fish.setImageUrl((String) data.get("imageUrl"));
+                fish.setDescription((String) data.get("description"));
+                fish.setWaterType((String) data.get("waterType"));
+                return fish;
+            }
+            throw new ResourceNotFoundException("Fish not found with id: " + id);
         } catch (HttpClientErrorException.NotFound e) {
             throw new ResourceNotFoundException("Fish not found with id: " + id);
         }
