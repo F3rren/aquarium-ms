0951aa3ffa7103d92334bebcace85d0975bca6b0
Samuele Alessandro Di Silvestri <samueledisilvestri@gmail.com>
2025-12-02 16:44:50 +0100

    fix: change ID generation strategy from IDENTITY to AUTO for Parameter entity

--- parameters/parameters-service/src/main/java/it/f3rren/aquarium/parameters_service/Model/Parameter.java
+++ parameters/parameters-service/src/main/java/it/f3rren/aquarium/parameters_service/Model/Parameter.java
@@ -16,7 +16,7 @@ import lombok.*;
 public class Parameter {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
     
