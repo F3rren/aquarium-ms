e296158388aca82c973744e6009d0ab8b2a96feb
Samuele Alessandro Di Silvestri <samueledisilvestri@gmail.com>
2025-12-18 09:49:32 +0100

    feat: implement Product management API with CRUD operations and filtering options

---
+++ maintenance/maintenance-service/src/main/java/it/f3rren/aquarium/maintenance_service/Model/ProductCategory.java
@@ -0,0 +1,12 @@
+package it.f3rren.aquarium.maintenance_service.model;
+
+public enum ProductCategory {
+    BACTERIA,
+    FOOD,
+    TEST,
+    SUPPLEMENT,
+    WATER_TREATMENT,
+    EQUIPMENT,
+    MEDICINE,
+    OTHER
+}
